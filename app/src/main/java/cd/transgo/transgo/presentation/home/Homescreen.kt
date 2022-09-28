349a445939b20ec4620df7e0eb0124ba4c1c87cb
Josh Muleshi <jmuleshi2@gmail.com>
2022-09-28 12:54:24 +0200

    basic language select done

--- app/src/main/java/cd/transgo/transgo/presentation/home/Homescreen.kt
+++ app/src/main/java/cd/transgo/transgo/presentation/home/Homescreen.kt
@@ -4,14 +4,22 @@ import ToolbarWidget
 import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import cd.transgo.transgo.R
 import cd.transgo.transgo.ui.theme.BackGray
 
 @Composable
@@ -19,6 +27,9 @@ fun Homescreen(navController: NavHostController) {
 
     val context = LocalContext.current
     //val posts by viewModel.data.collectAsState()
+    var isClicked by remember {
+        mutableStateOf(false)
+    }
 
     BackHandler(enabled = true) {
         (context as? Activity)?.finish()
@@ -31,13 +42,31 @@ fun Homescreen(navController: NavHostController) {
     ){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
             modifier = Modifier
                 .fillMaxSize()
                 .background(BackGray)
         ) {
-            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.TopCenter) {
-                Text(text = "Hello world !")
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp),
+                elevation = 1.dp
+            ) {
+                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                    Row(
+                        modifier = Modifier
+                            .padding(15.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = if (isClicked) "Lingala" else "Français")
+                        Icon(painter = painterResource(id = R.drawable.ic_sync_alt), contentDescription = "", modifier = Modifier
+                            .padding(horizontal = 40.dp)
+                            .clickable { isClicked = !isClicked })
+                        Text(text = if (isClicked) "Français" else "Lingala")
+                    }
+                }
             }
         }
     }
