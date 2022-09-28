d2960484fdb66a4270e2b7e44e9b0d1d1430ecd5
Josh Muleshi <jmuleshi2@gmail.com>
2022-09-28 09:35:46 +0200

    hilt and google services added

--- app/src/main/java/cd/transgo/transgo/MainActivity.kt
+++ app/src/main/java/cd/transgo/transgo/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import cd.transgo.transgo.app.navigation.SetupNavGraph
 import cd.transgo.transgo.ui.theme.TransGoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
