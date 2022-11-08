4deaf20ec156ba85d50fa204edb634a4efcd0f2b
Josh Muleshi <jmuleshi2@gmail.com>
2022-11-08 10:46:26 +0200

    last commit

--- app/src/main/java/cd/transgo/transgo/presentation/translator/TranslatorScreen.kt
+++ app/src/main/java/cd/transgo/transgo/presentation/translator/TranslatorScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -17,19 +19,33 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import cd.transgo.transgo.R
 import cd.transgo.transgo.presentation.translator.business.TopActionBar
 import cd.transgo.transgo.ui.theme.*
 
 @Composable
 fun TranslatorScreen(navController: NavController) {
+
     var title by remember { mutableStateOf("") }
+    val scaffoldState = rememberScaffoldState()
     val focusRequest = remember {
         FocusRequester()
     }
     Scaffold(
         topBar = {
             TopActionBar(navController)
-        }
+        }, floatingActionButton = {
+            FloatingActionButton(onClick = {
+                
+            },
+                backgroundColor = Purple500,
+                contentColor = Color.White
+            ) {
+                Icon(painter = painterResource(id = R.drawable.ic_translate), contentDescription = "Add note")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        scaffoldState = scaffoldState
     ){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -39,6 +55,7 @@ fun TranslatorScreen(navController: NavController) {
                 .background(BackGray)
         ) {
             Column(modifier = Modifier.fillMaxSize()) {
+
                 Text(
                     text = "Mbote na bino",
                     modifier = Modifier
@@ -49,6 +66,7 @@ fun TranslatorScreen(navController: NavController) {
                     fontStyle = FontStyle.Normal,
                     fontWeight = FontWeight.Bold
                 )
+
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -72,6 +90,44 @@ fun TranslatorScreen(navController: NavController) {
                             focusedIndicatorColor = Trans,
                             unfocusedIndicatorColor = Trans
                         )
+                    )
+                }
+
+                Text(
+                    text = "1/100",
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Medium
+                )
+
+                LinearProgressIndicator(progress = 0.5f, modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .height(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_back),
+                        contentDescription = "back",
+                        modifier = Modifier
+                            .size(40.dp),
+                        tint = Purple500
+                    )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_forward),
+                        contentDescription = "press",
+                        modifier = Modifier
+                            .size(40.dp),
+                        tint = Purple500
                     )
                 }
             }
